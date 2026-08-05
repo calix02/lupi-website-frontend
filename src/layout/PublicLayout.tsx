6d8cac655bf9e14535c7095a91a2c4177e3ccae9
Mark Angelo Alvarado <alvaradomarkangelo28@gmail.com>
2026-08-05 21:45:21 +0800

    feat: add keyboard shortcut for navigation to Login page in PublicLayout

--- src/layout/PublicLayout.tsx
+++ src/layout/PublicLayout.tsx
@@ -1,7 +1,24 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Footer from "@/components/Footer/Footer";
+import { useEffect } from "react";
 
 export default function PublicLayout() {
+    const navigate = useNavigate();
+
+    useEffect(() =>{
+        const handleKeyDown = (e: KeyboardEvent) =>{
+            if(e.ctrlKey && e.shiftKey && e.key.toLocaleLowerCase() === "l"){
+                e.preventDefault();
+                navigate("/login");
+            }
+
+        };
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+
+    }, [navigate]);
     return (
         <>
             <main>
